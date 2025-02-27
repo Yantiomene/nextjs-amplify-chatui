e9858babb5daa2eb2e59ad39ed3d15d12ec1cacb
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2025-02-27 18:22:35 +0100

    [Update] Added a Logo for the chatapp

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function IndexPage() {
         <div className="min-h-screen flex flex-col justify-center items-center">
           <header className="bg-white p-4 shadow-sm shadow-gray-300 w-full flex justify-between">
             <h1 className="text-2xl pt-1 font-bold bg-gradient-to-r text-transparent from-indigo-500 to-orange-500 bg-clip-text">
-              Chatty
+              YanChat
             </h1>
             <div className="flex items-center">
               <div className="mr-4 text-gray-600 hover:text-gray-80">
